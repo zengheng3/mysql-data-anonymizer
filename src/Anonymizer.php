132e60bd40dc8b397ea6397bebeff57e7cdbf82d
Heng <heng.zeng@globalis-ms.com>
2019-05-09 18:16:58 +0200

    Complete remote operation and all null callback on table

--- src/Anonymizer.php
+++ src/Anonymizer.php
@@ -11,11 +11,25 @@ use Globalis\MysqlDataAnonymizer\Helpers;
 class Anonymizer
 {
     /**
+     * whether fetch data from or deploy anonimized data to a serveur in distance
+     *
+     * @var DatabaseInterface
+     */
+    public $is_remote = false;
+
+    /**
      * Database interactions object.
      *
      * @var DatabaseInterface
      */
-    protected $mysql_pool;
+    protected $mysql_pool = null;
+
+    /**
+     * Remote database interactions object.
+     *
+     * @var DatabaseInterface
+     */
+    protected $mysql_pool_source = null;
 
     /**
      * Generator object (e.g \Faker\Factory).
@@ -43,12 +57,17 @@ class Anonymizer
      *
      * @param mixed             $generator
      */
-    public function __construct($generator = null)
+    public function __construct($is_remote = false, $generator = null)
     {
+        $this->is_remote = $is_remote;
     	$this->load_config();
         $this->load_helpers();
 
         $this->mysql_pool = Mysql\pool(Mysql\ConnectionConfig::fromString("host=".$this->config['DB_HOST'].";user=".$this->config['DB_USER'].";pass=".$this->config['DB_PASSWORD'].";db=". $this->config['DB_NAME']), $this->config['NB_MAX_MYSQL_CLIENT']);
+
+        if ($this->is_remote) {
+            $this->mysql_pool_source = Mysql\pool(Mysql\ConnectionConfig::fromString("host=".$this->config['DB_HOST_SOURCE'].";user=".$this->config['DB_USER_SOURCE'].";pass=".$this->config['DB_PASSWORD_SOURCE'].";db=". $this->config['DB_NAME_SOURCE']), $this->config['NB_MAX_MYSQL_CLIENT_SOURCE']);
+        }
 
         if (is_null($generator) && class_exists('\Faker\Factory')) {
             $generator = \Faker\Factory::create($this->config['DEFAULT_GENERATOR_LOCALE']);
@@ -66,16 +85,16 @@ class Anonymizer
             if (!file_exists(__DIR__ . "/../config/config.php")) {
                 throw new Exception('config.php not found in the directory.');
             }
-            $this->config = require __DIR__ . "/../config/config.php";
+            $config = require __DIR__ . "/../config/config.php";
 
              $this->config = [
-                'DB_HOST'                   => $this->config['DB_HOST'] ?? '127.0.0.1',
-                'DB_NAME'                   => $this->config['DB_NAME'] ?? '',
-                'DB_USER'                   => $this->config['DB_USER'] ?? '',
-                'DB_PASSWORD'               => $this->config['DB_PASSWORD'] ?? '',
-                'NB_MAX_MYSQL_CLIENT'       => $this->config['NB_MAX_MYSQL_CLIENT'] ?? 20,
-                'NB_MAX_PROMISE_IN_LOOP'    => $this->config['NB_MAX_PROMISE_IN_LOOP'] ?? 20,
-                'DEFAULT_GENERATOR_LOCALE'  => $this->config['DEFAULT_GENERATOR_LOCALE'] ?? 'en_US'
+                'DB_HOST'                   => $config['DB_HOST'] ?? '127.0.0.1',
+                'DB_NAME'                   => $config['DB_NAME'] ?? '',
+                'DB_USER'                   => $config['DB_USER'] ?? '',
+                'DB_PASSWORD'               => $config['DB_PASSWORD'] ?? '',
+                'NB_MAX_MYSQL_CLIENT'       => $config['NB_MAX_MYSQL_CLIENT'] ?? 20,
+                'NB_MAX_PROMISE_IN_LOOP'    => $config['NB_MAX_PROMISE_IN_LOOP'] ?? 20,
+                'DEFAULT_GENERATOR_LOCALE'  => $config['DEFAULT_GENERATOR_LOCALE'] ?? 'en_US'
              ];
 
             foreach ($this->config as $parameter => $value) {
@@ -93,6 +112,33 @@ class Anonymizer
             if (!filter_var($this->config['DB_HOST'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
                 throw new Exception('DB_HOST is not valid.');
             }
+
+            if ($this->is_remote) {
+                $remote_config = [
+                    'DB_HOST_SOURCE'                => $config['DB_HOST_SOURCE'] ?? '',
+                    'DB_NAME_SOURCE'                => $config['DB_NAME_SOURCE'] ?? '',
+                    'DB_USER_SOURCE'                => $config['DB_USER_SOURCE'] ?? '',
+                    'DB_PASSWORD_SOURCE'            => $config['DB_PASSWORD_SOURCE'] ?? '',
+                    'NB_MAX_MYSQL_CLIENT_SOURCE'    => $config['NB_MAX_MYSQL_CLIENT_SOURCE'] ?? 50,
+                ];
+
+                foreach ($remote_config as $parameter => $value) {
+                    if (!$value) {
+                        throw new Exception($parameter . ' can not be empty.');
+                        continue;
+                    }
+                    if ($parameter === 'NB_MAX_MYSQL_CLIENT_SOURCE' && !is_int($value)) {
+                        throw new Exception($parameter . ' should be integer.');
+                    }
+                }
+
+                if (!filter_var($remote_config['DB_HOST_SOURCE'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+                    throw new Exception('DB_HOST_SOURCE is not valid.');
+                }
+
+                $this->config = array_merge($this->config, $remote_config);
+            }
+
         } catch (Exception $e) {
             echo 'Exception: ' . $e->getMessage(). PHP_EOL;
             exit(1);
@@ -156,8 +202,6 @@ class Anonymizer
             $promise_count = 0;
             yield $this->disableForeignKeyCheck();
             foreach ($this->blueprints as $table => $blueprint) {
-                $blueprint = $this->filterAndBackUpForeignKeys($blueprint);
-
                 foreach ($blueprint->synchroColumns as $column_name => $data) {
                     yield $this->addUpdateTrigger($blueprint, $column_name, $data);
                 }
@@ -194,6 +238,70 @@ class Anonymizer
         });
     }
 
+
+    /**
+     * Perform data anonymization.
+     *
+     * @return void
+     */
+    public function remote_run()
+    {
+        Amp\Loop::run(function () {
+            $promises = [];
+            $promise_count = 0;
+            yield $this->disableForeignKeyCheck();
+            foreach ($this->blueprints as $table => $blueprint) {
+
+                $foreign_keys = yield $this->getRelatedForeignKeys(false, [$table]);
+                $exclude_foreign_keys = [];
+                while(yield $foreign_keys->advance()) {
+                    $exclude_foreign_keys[] = $this->getForeignKeyString($foreign_keys->getCurrent());
+                }
+
+                $create_table_request = yield $this->getCreateTableRequest($table);
+                if(yield $create_table_request->advance()) {
+                    $create_table_request = $create_table_request->getCurrent()['Create Table'];
+
+                    $create_table_request = str_replace($exclude_foreign_keys, '', $create_table_request);
+
+                    yield $this->mysql_pool->query('DROP TABLE IF EXISTS '. $table);
+                    yield $this->mysql_pool->query($create_table_request);
+                }
+
+                $selectData = yield $this->getSelectData($table, $blueprint, true);
+                $rowNum = 0;
+
+                //Update every line selected
+                while (yield $selectData->advance()) {
+                    $row = $selectData->getCurrent();
+                    $promises[] = $this->insertLine(
+                        $blueprint,
+                        Helpers\GeneralHelper::arrayOnly($row, $blueprint->primary),
+                        $blueprint->columns,
+                        $rowNum,
+                        $row);
+
+                    $promise_count ++;
+                    $rowNum ++;
+
+                    //Wait for all the results of SQL queries and clear the promise table
+                    if($promise_count > $this->config['NB_MAX_PROMISE_IN_LOOP']) {
+                        yield \Amp\Promise\all($promises);
+                        $promises = [];
+                        $promise_count = 0;
+                    }
+                }
+            }
+
+            $foreignKeys = yield $this->getRelatedForeignKeys(true);
+             while (yield $foreignKeys->advance()) {
+                $row = $foreignKeys->getCurrent();
+                yield $this->restoreForeignKey($row);
+            }
+        });
+    }
+
+
     /**
      * Describe a table with a given callback.
      *
@@ -202,9 +310,9 @@ class Anonymizer
      *
      * @return void
      */
-    public function table($name, callable $callback)
+    public function table($name, callable $callback = NULL)
     {
-        $blueprint = new Blueprint($name, $callback);
+        $blueprint = new Blueprint($name, $this->config['DB_NAME'], $callback);
 
         $this->blueprints[$name] = $blueprint->build();
     }
@@ -222,7 +330,7 @@ class Anonymizer
     {
         $value = $this->handlePossibleClosure($replace);
 
-        return addslashes($this->replacePlaceholders($value, $rowNum));
+        return $this->replacePlaceholders($value, $rowNum);
     }
 
     /**
@@ -288,6 +396,30 @@ class Anonymizer
     }
 
     /**
+     * (Remote operation only)
+     * Insert a line
+     *
+     * @param array $blueprint
+     * @param array $primaryKeyValues
+     * @param array $columns
+     * @param int $rowNum
+     * @param array $row
+     *
+     * @return promise
+     */
+    public function insertLine($blueprint, $primaryKeyValues, $columns, $rowNum, $row)
+    {
+        $set = $this->buildSetForArray($columns, $rowNum, $row);
+
+        $sql = "INSERT INTO 
+                    {$blueprint->table}
+                SET
+                    {$set}";
+
+        return $this->mysql_pool->query($sql);
+    }
+
+    /**
      * Get lines that need to be updated
      *
      * @param array $table
@@ -297,10 +429,14 @@ class Anonymizer
      */
     protected function getSelectData($table, $blueprint)
     {
-        foreach ($blueprint->columns as $column) {
-            if ($column['replaceByFields']) {
-                $columns = '*';
-                break;
+        if ($this->is_remote) {
+            $columns = '*';
+        } else {
+            foreach ($blueprint->columns as $column) {
+                if ($column['replaceByFields']) {
+                    $columns = '*';
+                    break;
+                }
             }
         }
 
@@ -311,6 +447,10 @@ class Anonymizer
 
         if($blueprint->globalWhere) {
             $sql .= " WHERE " . $blueprint->globalWhere;
+        }
+
+        if ($this->is_remote) {
+            return $this->mysql_pool_source->query($sql);
         }
 
         return $this->mysql_pool->query($sql);
@@ -347,14 +487,15 @@ class Anonymizer
         $set = [];
         foreach ($columns as $column) {
 
-            $originalData = $row[$column['name']];
-            if ($column['replaceByFields']) {
+            if (is_callable($column['replaceByFields'])) {
                 $row[$column['name']] = call_user_func($column['replaceByFields'], $row, $this->generator);
             }
 
             if ($column['replace']) {
                 $row[$column['name']] = $this->calculateNewValue($column['replace'], $rowNum);
             }
+
+            $row[$column['name']] = addslashes($row[$column['name']]);
 
             if (empty($column['where'])) {
                 $set[] = "{$column['name']}='{$row[$column['name']]}'";
@@ -367,28 +508,22 @@ class Anonymizer
             }
         }
 
-        return implode(' ,', $set);
-    }
+        if ($this->is_remote) {
 
-    /**
-     * Ignore all foreign keys which automatically update the values and save other foreign keys' info into an array 
-     *
-     * @param array $foreignKey
-     * @param Blueprint $blueprint
-     *
-     * @return Promise
-     */
-    protected function filterAndBackUpForeignKeys($blueprint)
-    {
-        foreach($blueprint->synchroColumns as $currentColumnName => &$synchroColumn) {
-            foreach ($synchroColumn as $index => &$column) {
-                if(!$column['database']) {
-                    $column['database'] = $this->config['DB_NAME'];
+            $updated_columns = array_column($columns, 'name');
+            foreach ($row as $name => $value) {
+                if(!in_array($name, $updated_columns)) {
+                    if(is_null($value)) {
+                        $set[] = "{$name} = NULL";
+                    } else {
+                        $value = addslashes($value);
+                        $set[] = "{$name} = '{$value}'";
+                    }
                 }
             }
         }
 
-        return $blueprint;
+        return implode(' ,', $set);
     }
 
     /**
@@ -437,9 +572,105 @@ class Anonymizer
     }
 
 
+    /**
+     * Disable the foreign key check for this sesssion
+     *
+     * @return Promise
+     */
     protected function disableForeignKeyCheck()
     {
         $sql = "SET FOREIGN_KEY_CHECKS=0;";
         return $this->mysql_pool->query($sql);
+    }
+
+    /**
+     * (Remote operation only)
+     * Get a query for creating an existing table
+     *
+     * @param string $table_name
+     *
+     * @return Promise
+     */
+    protected function getCreateTableRequest($table_name)
+    {
+        $sql = "SHOW CREATE TABLE {$table_name}";
+        return $this->mysql_pool_source->query($sql);
+    }
+
+    /**
+     * (Remote operation only)
+     * Restore a foreign key
+     *
+     * @param string $foreignKey
+     *
+     * @return Promise
+     */
+    protected function restoreForeignKey($foreignKey)
+    {
+        $table_name = $this->config['DB_NAME'] . '.' . $foreignKey['TABLE_NAME'];
+        $source_table_name = $this->config['DB_NAME'] . '.' . $foreignKey['REFERENCED_TABLE_NAME'];
+        $sql = "
+                ALTER TABLE {$table_name}
+                ADD CONSTRAINT {$foreignKey['CONSTRAINT_NAME']}
+                FOREIGN KEY ({$foreignKey['COLUMN_NAME']}) 
+                REFERENCES {$source_table_name}({$foreignKey['REFERENCED_COLUMN_NAME']}) 
+                ON DELETE {$foreignKey['DELETE_RULE']} 
+                ON UPDATE {$foreignKey['UPDATE_RULE']}
+        ";
+        return $this->mysql_pool->query($sql);
+    }
+
+    /**
+     * (Remote operation only)
+     * Get string parts to create a foreign key
+     *
+     * @param string $row
+     *
+     * @return string
+     */
+    protected function getForeignKeyString($row)
+    {
+        return ",\n  CONSTRAINT `{$row['CONSTRAINT_NAME']}` FOREIGN KEY (`{$row['COLUMN_NAME']}`) REFERENCES `{$row['REFERENCED_TABLE_NAME']}` (`{$row['REFERENCED_COLUMN_NAME']}`) ON DELETE {$row['DELETE_RULE']} ON UPDATE {$row['UPDATE_RULE']}";
+    }
+
+    /**
+     * (Remote operation only)
+     * Get the information of all foreign keys related to selected tables or all tables
+     *
+     * @param boolean $include_reference
+     * @param array   $tables
+     *
+     * @return Promise
+     */
+    protected function getRelatedForeignKeys($include_reference = false, $tables = null)
+    {
+        if(!$tables) {
+            $tables = array_column($this->blueprints, 'table');
+        }
+
+        $tables = "'" . implode("','", $tables) . "'";
+
+        $sql = "
+            SELECT
+                key_column.CONSTRAINT_NAME,
+                key_column.TABLE_SCHEMA,
+                key_column.TABLE_NAME,
+                key_column.COLUMN_NAME,
+                key_column.REFERENCED_TABLE_SCHEMA,
+                key_column.REFERENCED_TABLE_NAME,
+                key_column.REFERENCED_COLUMN_NAME,
+                referential_constraints.UPDATE_RULE,
+                referential_constraints.DELETE_RULE
+            FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE key_column
+            JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS referential_constraints ON key_column.CONSTRAINT_NAME = referential_constraints.CONSTRAINT_NAME
+            WHERE key_column.REFERENCED_TABLE_SCHEMA = '{$this->config['DB_NAME_SOURCE']}'
+                AND referential_constraints.UNIQUE_CONSTRAINT_SCHEMA = '{$this->config['DB_NAME_SOURCE']}'
+                AND key_column.TABLE_NAME IN ({$tables})
+        ";
+
+        if($include_reference) {
+            $sql .= " AND key_column.REFERENCED_TABLE_NAME IN ({$tables})";
+        }
+        return $this->mysql_pool_source->query($sql);
     }
 }
